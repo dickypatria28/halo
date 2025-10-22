8120b717d0afcf2dd7110ad04c86fb5c84a30619
dickypatria28 <dickypatria03@students.amikom.ac.id>
2025-10-22 11:37:08 +0700

    implement activity and intent navigation

---
+++ Register.kt
@@ -0,0 +1,26 @@
+package com.example.a3095_login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+        val et_RegUsername: EditText = findViewById(R.id.et_reg_username)
+        val et_RegPassword: EditText = findViewById(R.id.et_reg_password)
+        val et_RegUlangiPassword: EditText = findViewById(R.id.et_reg_ulangi_password)
+        val btn_Register: Button = findViewById(R.id.btn_register)
+        val btn_BackToLogin: Button = findViewById(R.id.btn_back_to_login)
+
+
+        btn_BackToLogin.setOnClickListener {
+            finish()
+        }
+    }
+}
